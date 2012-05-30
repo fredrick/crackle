3e63c6b81188144ee29cdaa549acf935f574ec97
Fredrick Galoso <fred@wayoutmind.com>
2012-05-29 21:11:44 -0500

    Use es5-shim Array.indexOf for browsers lacking support
    - https://github.com/kriskowal/es5-shim

--- crackle.js
+++ crackle.js
@@ -6,36 +6,33 @@
 
 (function($) {
   "use strict";
+  /**
+   * es5-shim | Array.indexOf
+   * https://github.com/kriskowal/es5-shim/blob/master/LICENSE
+   */
   if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
-      if (this == null) {
-        throw new TypeError();
-      }
-      var t = Object(this);
-      var len = t.length >>> 0;
-      if (len === 0) {
+    Array.prototype.indexOf = function indexOf(sought /*, fromIndex */ ) {
+      var self = toObject(this),
+      length = self.length >>> 0;
+
+      if (!length) {
         return -1;
       }
-      var n = 0;
-      if (arguments.length > 0) {
-        n = Number(arguments[1]);
-        if (n != n) { // shortcut for verifying if it's NaN
-          n = 0;
-        } else if (n != 0 && n != Infinity && n != -Infinity) {
-          n = (n > 0 || -1) * Math.floor(Math.abs(n));
-        }
+
+      var i = 0;
+      if (arguments.length > 1) {
+        i = toInteger(arguments[1]);
       }
-      if (n >= len) {
-        return -1;
-      }
-      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
-      for (; k < len; k++) {
-        if (k in t && t[k] === searchElement) {
-          return k;
+
+      // handle negative indices
+      i = i >= 0 ? i : Math.max(0, length + i);
+      for (; i < length; i++) {
+        if (i in self && self[i] === sought) {
+          return i;
         }
       }
       return -1;
-    }
+    };
   }
   var Crackle = {
     /**
